c258187e624bbaeeccf70bacf2bc6192070cc42d
cparish312 <54302513+cparish312@users.noreply.github.com>
2024-06-16 19:14:11 -0400

    res files

---
+++ hindsight_server/res/Preferences_template.kt
@@ -0,0 +1,22 @@
+package com.connor.hindsight.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object Preferences {
+    private const val PREF_FILE_NAME = "Hindsight"
+    lateinit var prefs: SharedPreferences
+
+    const val screenrecordingenabled = "ScreenRecordingEnabled"
+    const val recordwhenactive = "RecordWhenActive"
+    const val screenshotsperautoupload = "ScreenshotsPerAutoUpload"
+    const val apikey = "ApiKey"
+    const val localurl = "LocalUrl"
+    const val interneturl = "InternetUrl"
+
+    fun init(context: Context) {
+        prefs = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
+
+        PYTHON_CONFIG_INSERT_HERE
+    }
+}
