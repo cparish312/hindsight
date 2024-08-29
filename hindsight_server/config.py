da4ddaf796553eac16ae62a79c7790835cce2f08
cparish312 <54302513+cparish312@users.noreply.github.com>
2024-08-29 17:24:05 -0400

    data dir in hidden dir

--- hindsight_server/config.py
+++ hindsight_server/config.py
@@ -2,14 +2,14 @@ import os
 from pathlib import Path
 
 BASE_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
-DATA_DIR = os.path.join(BASE_DIR, "data")
-RAW_SCREENSHOTS_DIR = os.path.join(DATA_DIR, "raw_screenshots")
-SERVER_LOG_FILE = os.path.join(DATA_DIR, "hindsight_server.log")
-
 ANDROID_IDENTIFIERS_ALIAS_FILE = os.path.join(BASE_DIR, "res/android_identifiers.json")
 
 HOME = Path.home()
 HINDSIGHT_SERVER_DIR = HOME / ".hindsight_server"
+DATA_DIR = os.path.join(HINDSIGHT_SERVER_DIR, "data")
+RAW_SCREENSHOTS_DIR = os.path.join(DATA_DIR, "raw_screenshots")
+SERVER_LOG_FILE = os.path.join(DATA_DIR, "hindsight_server.log")
+
 API_KEY_FILE = HINDSIGHT_SERVER_DIR / "secret_api_key.txt"
 if os.path.exists(API_KEY_FILE):
     with open(API_KEY_FILE, 'r') as infile:
