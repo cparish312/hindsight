d820fee3a8f27b956849729875a3dec8d8bcb4e4
cparish312 <54302513+cparish312@users.noreply.github.com>
2024-09-04 19:48:23 -0400

    start server

--- start_server.sh
+++ start_server.sh
@@ -6,7 +6,7 @@ cd hindsight_server
 python server_backend.py &
 
 # Start the Gunicorn server
-uwsgi --https :6000,$HOME/.hindsight_server/server.crt,$HOME/.hindsight_server/server.key --workers 4 --wsgi-file run_server.py --callable app --master --enable-threads --py-autoreload 1
+uwsgi --https :6000,$HOME/.hindsight_server/server.crt,$HOME/.hindsight_server/server.key --workers 4 --wsgi-file run_server.py --callable app --master --enable-threads
 
 # Optional: If you want to ensure that if the Gunicorn server exits, the background Python process is also killed
 trap "kill $!" EXIT
