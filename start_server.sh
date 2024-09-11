236a4b629c241e2ae1e8770b4fb3b7a8f59c39d0
cparish312 <54302513+cparish312@users.noreply.github.com>
2024-09-11 10:54:42 -0400

    cleaner server exit

--- start_server.sh
+++ start_server.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+# Function to kill both server processes
+cleanup() {
+    echo "Cleaning up and exiting..."
+    kill -SIGTERM $PID_BACKEND $PID_SERVER
+    wait $PID_BACKEND $PID_SERVER  # Wait for both processes to exit before exiting the script
+    exit 0
+}
+
+# Trap SIGINT and SIGTERM signals and call cleanup
+trap cleanup SIGINT SIGTERM
+
 cd hindsight_server
 
 # Start the Python backend script in the background
 python server_backend.py &
+PID_BACKEND=$!  # Save the PID of the backend server
 
-# Start the flask server
-python run_server.py
+# Start the Flask server in the background
+python run_server.py &
+PID_SERVER=$!  # Save the PID of the Flask server
 
-trap "kill $!" EXIT
+# Wait for both processes
+wait $PID_BACKEND $PID_SERVER
