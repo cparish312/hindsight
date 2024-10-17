e628d39202cc749db8152b9f03a6dca295e820ea
Nikita Kiriy <nikiriy@gmail.com>
2024-10-16 22:14:14 -0400

    corner case fixes (#19)
    * fix corner cases
    
    * remove some redundant logic andreset notebook file changes

--- hindsight_applications/hindsight_feed/app.py
+++ hindsight_applications/hindsight_feed/app.py
@@ -31,6 +31,8 @@ def get_feed_content():
     content = feed_generator.get_contents()
     for c in content:
         if c.published_date:
+            # print(c)
+            # print(c.published_date)
             c.published_date = pd.to_datetime(c.published_date / 1000, unit='s', utc=True).strftime('%Y-%m-%d')
     return content
 
